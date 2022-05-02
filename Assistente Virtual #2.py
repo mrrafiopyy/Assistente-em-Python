831ef100e70510dc3f6e8308bd0911dd0cdbe7a6
IndexErr0r <96931606+IndexErr0r@users.noreply.github.com>
2022-05-02 18:53:59 -0300

    Add files via upload
    Versão 2 da Assistente, agora com Interface Gráfica e voz aprimorada.

---
+++ Assistente Virtual #2.py
@@ -0,0 +1,17 @@
+import tkinter
+
+def ia():
+    # Importando módulos
+    import datetime
+    import speech_recognition as sr
+    import sounddevice as sd
+    import wavio as wv
+    import wikipedia
+    import webbrowser
+    import random
+    from gtts import gTTS
+    from playsound import playsound
+    import pywhatkit
+    import bs4
+    import requests
+    import joblib
